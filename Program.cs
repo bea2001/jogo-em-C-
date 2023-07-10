248525a4e06102e435f4137536e2ecd06d8cf113
Beatriz <beatrizrios2001@hotmail.com>
2023-07-10 11:08:59 -0300

    jogo em C#

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿Random aleatorio = new Random();
+int numeroSecreto = aleatorio.Next(1, 101);
+
+do{
+    Console.WriteLine("Digite um número entre 1 e 100: ");
+    int chute = int.Parse(Console.ReadLine());
+
+    if (chute == numeroSecreto){
+        Console.WriteLine("O jogo acabou. Você acertou o número secreto!");
+        break;       
+    } else if (chute < numeroSecreto) {
+        Console.WriteLine("O número é maior.");
+    } else {
+        Console.WriteLine("O número é menor.");
+    }
+
+} while (true);
